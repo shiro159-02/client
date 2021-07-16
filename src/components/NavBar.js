00f4ea45be85dbd56389f82fa9f9225601cc7c00
shiro1666 <hanbii135@gmail.com>
2021-07-16 15:30:06 +0300

    HI

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavBar, Nav} from "react-bootstrap";
+// import Nav from "react-bootstrap/Nav";
+
+const NavBarV = () => {
+  return (
+
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+  </Navbar>
+  );
+};
+
+export default NavBarV;
---
+++ src/pages/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarV from '../components/NavBar';
+const Shop = () => {
+  return (
+
+
+    <NavBarV />
+
+  );
+};
+
+export default Shop;
